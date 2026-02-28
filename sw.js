d59cfcbb4e7e2aaa26ca7939ea31ad2e33406f94
epicmotionSD <sdexecutionog@gmail.com>
2026-02-27 20:49:15 -0600

    Update class start date to March 30 and schedule to Mon-Wed 9am-4pm

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'kelatic-qr-v1';
+const urlsToCache = [
+  '/qr-poster.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700;800&family=Inter:wght@400;500;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/qr-poster.html'))
+  );
+});
